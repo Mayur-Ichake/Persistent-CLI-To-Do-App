6daa955b03fa5d7725f93032d8a2501935dbd2f8
Mayur Ichake <mayurichake4@gmail.com>
2025-10-22 19:57:47 +0530

    Done

--- todo.py
+++ todo.py
@@ -5,6 +5,7 @@
  2.Implement add/remove/view functionality
  3.Store tasks in a text file using open()"""
 
+# Load tasks from todo_list.txt file
 def load_tasks(filename):
     try:
         with open(filename, 'r') as file:
@@ -13,21 +14,29 @@ def load_tasks(filename):
     except FileNotFoundError:
         tasks = []
     return tasks
+
+# save tasks to a file
 def save_tasks(filename, tasks):
     with open(filename, 'w') as file:
         for task in tasks:
             file.write(task + '\n')
+
+# add a new task
 def add_task(tasks):
     task = input("Enter a new task: ")
     tasks.append(task)
     print(f'Task "{task}" added.')
+
+# view all tasks
 def view_tasks(tasks):
     if not tasks:
         print("No tasks in the to-do list.")
     else:
         print("To-Do List:")
-        for idx, task in enumerate(tasks, start=1):
-            print(f"{idx}. {task}")
+        for todo, task in enumerate(tasks, start=1):
+            print(f"{todo}. {task}")
+
+# delete a task
 def delete_task(tasks):
     view_tasks(tasks)
     try:
@@ -39,21 +48,25 @@ def delete_task(tasks):
             print("Invalid task number.")
     except ValueError:
         print("Please enter a valid number.")
+
+# main function to run the to-do list application
 def main():
-    filename = 'todo_list.txt'
+    filename = 'todo_list.txt'           # save tasks in this file
     tasks = load_tasks(filename)
     while True:
         print("\nTo-Do List Application")
-        print("1. View Tasks")
-        print("2. Add Task")
+        print("1. Add Task")
+        print("2. View Tasks")
         print("3. Delete Task")
         print("4. Exit")
+
+        # take user input
         choice = input("Choose an option (1-4): ")
         if choice == '1':
-            view_tasks(tasks)
-        elif choice == '2':
             add_task(tasks)
             save_tasks(filename, tasks)
+        elif choice == '2':
+            view_tasks(tasks)
         elif choice == '3':
             delete_task(tasks)
             save_tasks(filename, tasks)
@@ -62,6 +75,7 @@ def main():
             break
         else:
             print("Invalid choice. Please try again.")
+
 if __name__ == "__main__":
     main()
 
